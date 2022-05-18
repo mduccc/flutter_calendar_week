1bb309e375018169d1c5295d6809e4f50f1f4385
Minh Duc <mducc1412@gmail.com>
2022-05-18 22:00:04 +0700

    Support more scroll behavior (#35)
    * Fix analyzer
    
    * Support more scroll behavior

---
+++ lib/src/custom_scroll_behaiver.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+
+class CustomScrollBehavior extends MaterialScrollBehavior {
+  @override
+  Set<PointerDeviceKind> get dragDevices => {
+        PointerDeviceKind.mouse,
+        PointerDeviceKind.touch,
+        PointerDeviceKind.trackpad,
+        PointerDeviceKind.stylus,
+        PointerDeviceKind.invertedStylus,
+      };
+}
