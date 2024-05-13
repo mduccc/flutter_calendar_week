ebf708899c04b2fdb8827a28955c3fe118648a0f
Kenn <kakomea@yahoo.com>
2024-05-13 17:44:51 +0100

    localisation

---
+++ lib/generated/l10n.dart
@@ -0,0 +1,156 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'intl/messages_all.dart';
+
+// **************************************************************************
+// Generator: Flutter Intl IDE plugin
+// Made by Localizely
+// **************************************************************************
+
+// ignore_for_file: non_constant_identifier_names, lines_longer_than_80_chars
+// ignore_for_file: join_return_with_assignment, prefer_final_in_for_each
+// ignore_for_file: avoid_redundant_argument_values
+
+class S {
+  S();
+
+  static S? _current;
+
+  static S get current {
+    assert(_current != null, 'No instance of S was loaded. Try to initialize the S delegate before accessing S.current.');
+    return _current!;
+  }
+
+  static const AppLocalizationDelegate delegate =
+    AppLocalizationDelegate();
+
+  static Future<S> load(Locale locale) {
+    final name = (locale.countryCode?.isEmpty ?? false) ? locale.languageCode : locale.toString();
+    final localeName = Intl.canonicalizedLocale(name); 
+    return initializeMessages(localeName).then((_) {
+      Intl.defaultLocale = localeName;
+      final instance = S();
+      S._current = instance;
+ 
+      return instance;
+    });
+  } 
+
+  static S of(BuildContext context) {
+    final instance = S.maybeOf(context);
+    assert(instance != null, 'No instance of S present in the widget tree. Did you add S.delegate in localizationsDelegates?');
+    return instance!;
+  }
+
+  static S? maybeOf(BuildContext context) {
+    return Localizations.of<S>(context, S);
+  }
+
+  /// `Hello World!`
+  String get helloWorld {
+    return Intl.message(
+      'Hello World!',
+      name: 'helloWorld',
+      desc: 'The conventional newborn programmer greeting',
+      args: [],
+    );
+  }
+
+  /// `M`
+  String get m {
+    return Intl.message(
+      'M',
+      name: 'm',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `T`
+  String get t {
+    return Intl.message(
+      'T',
+      name: 't',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `W`
+  String get w {
+    return Intl.message(
+      'W',
+      name: 'w',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Th`
+  String get th {
+    return Intl.message(
+      'Th',
+      name: 'th',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `F`
+  String get f {
+    return Intl.message(
+      'F',
+      name: 'f',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `S`
+  String get s {
+    return Intl.message(
+      'S',
+      name: 's',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `S`
+  String get su {
+    return Intl.message(
+      'S',
+      name: 'su',
+      desc: '',
+      args: [],
+    );
+  }
+}
+
+class AppLocalizationDelegate extends LocalizationsDelegate<S> {
+  const AppLocalizationDelegate();
+
+  List<Locale> get supportedLocales {
+    return const <Locale>[
+      Locale.fromSubtags(languageCode: 'en'),
+      Locale.fromSubtags(languageCode: 'zh'),
+    ];
+  }
+
+  @override
+  bool isSupported(Locale locale) => _isSupported(locale);
+  @override
+  Future<S> load(Locale locale) => S.load(locale);
+  @override
+  bool shouldReload(AppLocalizationDelegate old) => false;
+
+  bool _isSupported(Locale locale) {
+    for (var supportedLocale in supportedLocales) {
+      if (supportedLocale.languageCode == locale.languageCode) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
